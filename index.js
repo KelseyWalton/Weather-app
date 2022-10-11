111560f572aaf67934e312d5c2874c381aef6de8
KelseyWalton <110851437+KelseyWalton@users.noreply.github.com>
2022-10-11 14:06:24 -0500

    added forecast element code

--- index.js
+++ index.js
@@ -26,7 +26,31 @@ function formatDate(date) {
 function displayForecast(){
   let forecastElement = document.querySelector("forecast");
   
-  forecastElement.innerHTML = ``;
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat",]
+  days.forEach(function(day) {
+  forecastHTML = forecast + 
+    `
+            <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="https://cdn-icons-png.flaticon.com/128/1163/1163661.png"
+                alt=""
+                width="36"
+                      />
+           <div class="weather-forecast-temperatures">
+               <span class="weather-forecast-temperature-max">
+                   50°</span>
+               <span class="weather-forecast-temperature-min">
+                   47°</span>
+                        </div>
+                    </div>
+                </div>
+                `;
+  };
+  forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+
 }
 function search(event) {
   event.preventDefault();
@@ -93,6 +117,8 @@ function displayCelciusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
